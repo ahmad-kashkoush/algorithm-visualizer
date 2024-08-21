f952d592dba924ecec33404b8501e9a6313f83d7
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-08-21 18:54:33 +0300

    feat: add bubble sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useBubbleSort } from "@/hooks/useBubbleSort";
+import { useSelectionSort } from "@/hooks/useSelectionSort";
 import ArrayInput from "@/ui/ArrayInput";
 import ArrayWrapper from "@/ui/ArrayWrapper";
 
@@ -6,33 +8,47 @@ import { useState } from "react";
 function App() {
   const [arr, setArr] = useState([5, 4, 3, 2, 1]);
   const [algo, setAlgo] = useState(""); // contains algo key
+  const { handleBubbleSort, active: activeBubble } = useBubbleSort({
+    arr: arr,
+    updateArray: setArr,
+  });
+
+  const { handleSelectionSort, active: activeSelection } = useSelectionSort({
+    arr: arr,
+    updateArray: setArr,
+  });
+
   const activeMapper = {
     bubble: activeBubble,
     selection: activeSelection,
-
   };
 
   function handleEnterArray(arr) {
     setArr(arr);
   }
   return (
-    <div className="w-screen">
+    <div className="w-screen ">
       <ArrayInput handleEnterArray={handleEnterArray} />
       <ArrayWrapper arr={arr} active={activeMapper[algo] || []} />
       <div className="actions mx-auto mt-8 w-fit space-x-4">
         <button
           data-id="bubble"
+          onClick={(e) => {
+            handleBubbleSort(e);
+            setAlgo(e.target.dataset.id);
+          }}
         >
           Bubble sort
         </button>
         <button
           data-id="selection"
+          onClick={(e) => {
+            handleSelectionSort(e);
+            setAlgo(e.target.dataset.id);
+          }}
         >
           Selection sort
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
     </div>
   );
---
+++ src/hooks/useBubbleSort.js
@@ -0,0 +1,44 @@
+import { DELAY_TIME } from "@/utils/constans";
+import { clearTimeouts, delay } from "@/utils/helpers";
+import { useState } from "react";
+
+
+// todo: know how to stop updating in cases like enter new array or clicking on stop button
+function useBubbleSort({ arr, updateArray }) {
+    const [active, setActive] = useState([]);
+    function handleBubbleSort(e) {
+        e.preventDefault();
+        (async function () {
+            const a = [...arr];
+            const n = a.length;
+            for (let i = 0; i < n; i++) {
+                let swapped = false;
+                // number from n-i-1 to n-1 are sorted
+                for (let j = 0; j < n - i - 1; j++) {
+                    setActive([
+                        ...Array.from({ length: n - 1 - (n - i - 1) + 1 }, (_, index) => ({ index: n - index , color: "bg-yellow-500" })),
+                        { index: j, color: "bg-red-500" },
+                        { index: j + 1, color: "bg-green-500" }
+
+                    ]);
+                    // await delay(DELAY_TIME);
+                    if (a[j] > a[j + 1]) {
+                        [a[j], a[j + 1]] = [a[j + 1], a[j]];
+                        swapped = true;
+                        updateArray(() => a);
+                        await delay(DELAY_TIME);
+                    }
+                }
+                if (!swapped) {
+                    setActive([]);
+                    return;
+                }
+            }
+            updateArray(() => a);
+        })();
+    }
+
+
+    return { handleBubbleSort, active };
+}
+export { useBubbleSort };
