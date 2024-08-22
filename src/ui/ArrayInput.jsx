d22590a3b39d7550d5fa91ad87372b033a012d06
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-08-22 15:48:13 +0300

    refactor: update array input
    - removed entering custom array for better UX
    - added customezed length of array

--- src/ui/ArrayInput.jsx
+++ src/ui/ArrayInput.jsx
@@ -1,40 +1,30 @@
 import { useState } from "react";
 
 function ArrayInput({ handleEnterArray }) {
-  const [arrayString, setArrayString] = useState("1,3,2,4");
-  function handleSubmit(e) {
-    // todo validation
-    e.preventDefault();
-    const arr = arrayString.split(",").map((item) => Number(item));
-    handleEnterArray(arr);
-    setArrayString("");
-  }
+  const [length, setLength] = useState(10);
   function handleSubmitRandom(e) {
     e.preventDefault();
-    const arrayLength = Math.floor(Math.random() * (30 - 5) + 5);
-    const getRandomInt = (max) => Math.floor(Math.random() * max) + 1;
-    // make array Unique
     let arr = [];
-    while (arr.length < arrayLength) {
-      const randomInt = getRandomInt(100);
-      if (!arr.includes(randomInt)) arr.push(randomInt);
-    
+    while (arr.length < length) {
+      let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+      if (!arr.includes(randomNumber)) arr.push(randomNumber);
+     
     }
     handleEnterArray(arr);
   }
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="enter numbers seperated by ,"
-          defaultValue={arrayString}
-          onChange={(e) => setArrayString(e.target.value)}
-        />
-        <button type="submit">submit</button>
-      </form>
+    <div className=" flex justify-center items-center gap-4 mb-8">
+      <span>Length: {length}</span>
+      <input
+        type="range"
+        max={20}
+        min={5}
+        value={length}
+        onChange={(e) => setLength(Number(e.target.value))}
+      />
       <button onClick={handleSubmitRandom}>Random</button>
-    </>
+    </div>
   );
 }
 export default ArrayInput;
