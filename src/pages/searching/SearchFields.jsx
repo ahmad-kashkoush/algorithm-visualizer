8470572804207d132e7ab2c472b99c6128ccc583
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-09-18 17:57:20 +0300

    feat(Search): add Seach fields

--- src/pages/searching/SearchFields.jsx
+++ src/pages/searching/SearchFields.jsx
@@ -1,8 +1,35 @@
-function SearchFields(){
-    return (
-        <div>
-            SearchFields
-        </div>
-    );
+import ArrayInput from "@/ui/ArrayInput";
+import ArrayWrapper from "@/ui/ArrayWrapper";
+import { useState } from "react";
+
+function SearchFields({ useHandler, sortEnteredArray=false }) {
+  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
+  const [target, setTarget] = useState(3);
+  const { handleSearch, active } = useHandler({
+    arr: arr,
+    updateArray: setArr,
+    target: target,
+  });
+
+  return (
+    <>
+      <input
+        type="number"
+        value={target}
+        onChange={(e) => setTarget(+e.target.value)}
+      />
+      <ArrayInput handleEnterArray={setArr} isSorted={sortEnteredArray} />
+      <ArrayWrapper arr={arr} active={active} />
+      <div className="actions mx-auto mt-8 w-fit space-x-4">
+        <button
+          onClick={(e) => {
+            handleSearch(e);
+          }}
+        >
+          Sort{" "}
+        </button>
+      </div>
+    </>
+  );
 }
 export default SearchFields;
